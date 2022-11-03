601e17b3ed909e22be07cf01b9269609147a61a7
digis01 <iyanelimm@gmail.com>
2022-11-02 22:39:38 -0600

    Initial Commit

---
+++ ConsumoAPI/ConsumoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumoAPIApp.swift
+//  ConsumoAPI
+//
+//  Created by IYMM on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ConsumoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
